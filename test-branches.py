43af35a7c7f6e4c6ae10ed03b31aabcb78f0feb1
Anderson Kmetiuk <58405194+andersonkmetiuk@users.noreply.github.com>
2023-05-06 14:55:11 -0300

    updated for feature
    updated for feature

--- test-branches.py
+++ test-branches.py
@@ -1,5 +1,10 @@
-#BRANCH DEVELOP
+#BRANCH DEVELOP - Feature For
 
 #Python Test
-print('Branch Develop')
+print('\t Feature For \n')
+
+for i in range(5):
+    print(f'{i}...')
+
+print('end\n\n\n')
 
