69a0e2ab8fba6393d367b27d9368d128ceb0d4e3
Anderson Kmetiuk <58405194+andersonkmetiuk@users.noreply.github.com>
2023-05-06 15:34:20 -0300

    pullreq 2
    pullreq 2

--- test-branches.py
+++ test-branches.py
@@ -1,11 +1,11 @@
-#BRANCH DEVELOP - Feature Print
-
 #Python Test
 print('\t HELLO WORLD \n\n')
 
 print('\t TEST BRANCH \n\n')
 
 print('\t PRINT FEATURE \n\n')
+
+print('\t Pull request 2 \n\n')
 
 print('END! \n\n')
 
